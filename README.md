fb68a61d4bcb52ac8dd50865d24c40d66daf4791
Viacheslav Shago <vyacheslav.shago@gmail.com>
2022-01-08 11:38:04 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Java CI with Gradle](https://github.com/vshago/spring-demo-apigateway/actions/workflows/gradle.yml/badge.svg)](https://github.com/vshago/spring-demo-apigateway/actions/workflows/gradle.yml)
 # spring-demo-apigateway
+Demo application
---
+++ src/main/java/com/example/apigateway/SpringDemoApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApigatewayApplication.class, args);
+	}
+
+}
